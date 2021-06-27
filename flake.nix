fdd4a3ae54190e92c08eda1c25f92a47193ab133
Julie B. <no-reply@bbjubjub.fr>
2021-06-27 11:00:22 +0200

    add devShell

--- flake.nix
+++ flake.nix
@@ -1,21 +1,27 @@
 {
   description = "guest NixOS images with minimal footprint";
 
+  inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = inputs@{ self, nixpkgs, flake-utils }:
+  outputs = inputs@{ self, nixpkgs, devshell, flake-utils }:
     with flake-utils.lib;
     {
       nixosModules.miniguest = import modules/miniguest.nix inputs;
-    } //
-    eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-miniguest = pkgs.callPackage ./miniguest { };
-      in
-      rec {
-        defaultPackage = miniguest;
-        packages = { inherit miniguest; };
-        defaultApp = mkApp { drv = miniguest; };
-      });
+    } // simpleFlake {
+      inherit self nixpkgs;
+      name = "miniguest";
+      systems = defaultSystems;
+      preOverlays = [ devshell.overlay ];
+      overlay = final: prev: {
+        miniguest = rec {
+          miniguest = final.callPackage ./miniguest { };
+          defaultPackage = miniguest;
+          defaultApp = mkApp { drv = miniguest; };
+          devShell = final.devshell.mkShell {
+            imports = [ (final.devshell.importTOML ./devshell.toml) ];
+          };
+        };
+      };
+    };
 }
