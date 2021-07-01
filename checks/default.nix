5f7a244991364060df0aee231eea561f952080de
Julie B. <no-reply@bbjubjub.fr>
2021-07-01 11:43:48 +0200

    add git hooks

--- checks/default.nix
+++ checks/default.nix
@@ -1,21 +1,23 @@
 inputs@{ self, nixpkgs, ... }:
 final: prev:
 
-	let
-	kvm_guest = nixpkgs.lib.nixosSystem {
-	inherit (final) system;
-		modules = [
-		self.nixosModules.miniguest
-		{
-		boot.miniguest.enable = true;
-		boot.loader.grub.enable = false;
-		fileSystems."/" = {
-		    device = "none";
-    fsType = "tmpfs";
-    options = [ "defaults" "mode=755" ];
-};
-		} ];
-	};
-	in final.lib.optionalAttrs final.stdenv.isLinux {
-	build_kvm_guest = kvm_guest.config.system.build.miniguest;
+let
+  kvm_guest = nixpkgs.lib.nixosSystem {
+    inherit (final) system;
+    modules = [
+      self.nixosModules.miniguest
+      {
+        boot.miniguest.enable = true;
+        boot.loader.grub.enable = false;
+        fileSystems."/" = {
+          device = "none";
+          fsType = "tmpfs";
+          options = [ "defaults" "mode=755" ];
+        };
+      }
+    ];
+  };
+in
+final.lib.optionalAttrs final.stdenv.isLinux {
+  build_kvm_guest = kvm_guest.config.system.build.miniguest;
 }
