93b295a19126382e2abea475e4289556951a6d63
Julie B. <no-reply@bbjubjub.fr>
2021-07-02 22:16:52 +0200

    add list of commands

--- miniguest/main_arg.bash
+++ miniguest/main_arg.bash
@@ -1,4 +1,5 @@
 # ARG_POSITIONAL_SINGLE(command, subcommand to run)
+# ARG_TYPE_GROUP_SET(commands, COMMAND, command, [install,help])
 # ARG_OPTIONAL_SINGLE(guests-dir, , directory containing guests profiles, /etc/miniguests)
 # ARG_LEFTOVERS(subcommand arguments)
 # ARGBASH_GO
