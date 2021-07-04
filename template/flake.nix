aff123e2017df22d72f06eb006cdbc0997205ba8
Julie B. <no-reply@bbjubjub.fr>
2021-07-04 21:14:46 +0200

    add devShell to template

--- template/flake.nix
+++ template/flake.nix
@@ -5,14 +5,22 @@
     nixos.url = "nixpkgs/nixos-unstable";
     miniguest.url = "github:bbjubjub2494/miniguest";
     miniguest.inputs.nixpkgs.follows = "nixos";
+    flake-utils.follows = "miniguest/flake-utils";
   };
 
-  outputs = { self, miniguest, nixos }:
+  outputs = { self, nixos, miniguest, flake-utils }:
     with nixos.lib; {
       nixosConfigurations = attrsets.genAttrs [ "stateless" ] (name:
         nixosSystem {
           system = "x86_64-linux";
           modules = [ miniguest.nixosModules.miniguest (./guests + "/${name}") ];
         });
-    };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      with import nixos
+        {
+          inherit system;
+          overlays = [ miniguest.overlay ];
+        }; {
+        devShell = mkShell { buildInputs = [ miniguest ]; };
+      });
 }
