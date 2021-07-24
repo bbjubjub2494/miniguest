b869e0473d177490dafcc239dd15ac5739da904b
Julie B. <no-reply@bbjubjub.fr>
2021-07-24 17:23:44 +0200

    relocate profiles under /nix/var/

--- miniguest/install.bash
+++ miniguest/install.bash
@@ -3,4 +3,6 @@ source @out@/lib/install_arg.bash
 parse_flake_reference "$_arg_flake_reference"
 
 mkdir -p "$guests_dir" || die "" $?
-run_nix build --profile "$guests_dir/$guest_name" "$flake#nixosConfigurations.$guest_name.config.system.build.miniguest" || die "unable to build guest!" $?
+mkdir -p "$profiles_dir" || die "" $?
+run_nix build --profile "$profiles_dir/$guest_name" "$flake#nixosConfigurations.$guest_name.config.system.build.miniguest" || die "unable to build guest!" $?
+ln -sf "$profiles_dir/$guest_name" "$guests_dir" || die "" $?
--- miniguest/main.bash
+++ miniguest/main.bash
@@ -3,6 +3,7 @@
 source @out@/lib/main_arg.bash
 
 guests_dir="$_arg_guests_dir"
+profiles_dir="/nix/var/nix/profiles/miniguest-profiles"
 nix="$_arg_nix"
 
 source @out@/lib/functions.bash
