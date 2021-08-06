edf2a6587db3102939ab255e82d052a35814c2fb
Julie B. <no-reply@bbjubjub.fr>
2021-08-06 21:50:56 +0200

    relicense
    My specifications:
    - Nix code should receive a permissive license, in part not to copyleft
    away everyone's NixOS configurations for no good reason.
    - The companion tool should be protected by copyleft.
    - The templates should immediately rise to the public domain.
    - It should be very clear which part falls under which license.
    
    Based on that, I have selected the ISC and BSD-0 because they are short,
    and the GNU General Public License version 3 or later as a copyleft
    license.

--- miniguest/functions.bash
+++ miniguest/functions.bash
@@ -1,3 +1,20 @@
+# Copyright 2021 Julie Bettens
+#
+# This file is part of the Miniguest companion.
+#
+# The Miniguest companion is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Miniguest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Miniguest.  If not, see <https://www.gnu.org/licenses/>.
+
 function run_nix {
 	command "$nix" --experimental-features "nix-command flakes" "$@"
 }
--- miniguest/install.bash
+++ miniguest/install.bash
@@ -1,3 +1,20 @@
+# Copyright 2021 Julie Bettens
+#
+# This file is part of the Miniguest companion.
+#
+# The Miniguest companion is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Miniguest is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Miniguest.  If not, see <https://www.gnu.org/licenses/>.
+
 source install_arg.bash
 
 parse_flake_reference "$_arg_flake_reference"
