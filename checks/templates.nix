444aff991fe9c8e7ebfb55866fdc58932710b3bf
Julie B. <no-reply@bbjubjub.fr>
2021-08-08 19:14:18 +0200

    Add check for lxc container template

---
+++ checks/templates.nix
@@ -0,0 +1,43 @@
+inputs@{ self, nixpkgs, ... }: system:
+
+with nixpkgs.legacyPackages.${system};
+with self.packages.${system};
+let
+in
+lib.optionalAttrs stdenv.isLinux {
+  create_container = nixosTest {
+    name = "miniguest-create-container";
+    machine = {
+      imports = [ self.nixosModules.declarative ];
+
+      virtualisation.lxc.enable = true;
+      users.users.root = {
+        subUidRanges = [{ startUid = 1000000; count = 65536; }];
+        subGidRanges = [{ startGid = 1000000; count = 65536; }];
+      };
+
+      miniguests.container.configuration = {
+        boot.miniguest.enable = true;
+        boot.miniguest.guestType = "lxc";
+        boot.miniguest.storeCorruptionWarning = false;
+      };
+      environment.systemPackages = [ miniguest ];
+
+      virtualisation.memorySize = 1024;
+    };
+    testScript = ''
+      machine.succeed("""
+      cat >extra-config <<EOF
+      lxc.idmap = u 0 1000000 65536
+      lxc.idmap = g 0 1000000 65536
+      EOF
+      """);
+      machine.succeed("""
+      miniguest create -t lxc container | sh
+      """);
+      machine.succeed("""
+      lxc-start container
+      """);
+    '';
+  };
+}
