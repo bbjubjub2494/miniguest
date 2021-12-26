84446081b8259a3bda8d4c13edb40a5e2d32a34f
Louis Bettens <louis@bettens.info>
2021-12-26 20:13:26 +0100

    add test

--- checks/declarative-management.nix
+++ checks/declarative-management.nix
@@ -24,8 +24,34 @@ let
       }
     ];
   };
+  declarative_host_cross = if system != "x86_64-linux" then null else
+  nixosSystem {
+    inherit system;
+    modules = [
+      self.nixosModules.declarative
+      {
+        boot.isContainer = true;
+        miniguests.virtual-machine.configuration = {
+          boot.miniguest.enable = true;
+
+          fileSystems."/" = {
+            device = "none";
+            fsType = "tmpfs";
+            options = [ "defaults" "mode=755" ];
+          };
+        };
+        miniguests.container.system = "i686-linux";
+        miniguests.container.configuration = {
+          boot.miniguest.enable = true;
+          boot.miniguest.guestType = "lxc";
+        };
+      }
+    ];
+  };
 in
 with nixpkgs.legacyPackages.${system};
 optionalAttrs stdenv.isLinux {
   build_declarative_host = declarative_host.config.system.build.toplevel;
+} // optionalAttrs (stdenv.isLinux && stdenv.isx86_64) {
+  build_declarative_host_cross = declarative_host.config.system.build.toplevel;
 }
