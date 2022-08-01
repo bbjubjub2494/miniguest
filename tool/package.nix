cda2a8096b40dada00ab5a2f6b16d20cbfa57e13
Julie B. <no-reply@bbjubjub.fr>
2022-08-01 16:52:21 +0200

    tool: bump version

--- tool/package.nix
+++ tool/package.nix
@@ -16,7 +16,7 @@
 
 stdenvNoCC.mkDerivation {
   pname = "miniguest";
-  version = "0.1.2";
+  version = "0.1.3";
   src = builtins.path { name = "source"; path = ./.; };
   inherit bash nixFlakes;
   lxc_template = miniguest-lxc-template;
